1b0727c7fc529fbb066652032eea10627e936652
LongDang136i <danglong136i@gmail.com>
2022-06-10 10:06:59 +0700

    Add project files.

---
+++ BookStore.Utilities/Constants/SystemConstants.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStore.Utilities.Constants
+{
+    public class SystemConstants
+    {
+        public const string MainConnectionString = "BookStoreSolutionDb";
+
+        public class AppSettings
+        {
+            public const string DefaultLanguageId = "DefaultLanguageId";
+
+            public const string Token = "Token";
+
+            public const string BaseAddress = "BaseAddress";
+        }
+
+        public class ProductSettings
+        {
+            public const int NumberOfFeaturedProducts = 8;
+            public const int NumberOfLatestProducts = 12;
+        }
+
+        public class ProductConstants
+        {
+            public const string NA = "N/A";
+        }
+    }
+}
---
+++ BookStore.Utilities/Exceptions/EShopException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStore.Utilities.Exceptions
+{
+    public class BookStoreException : Exception
+    {
+        public BookStoreException()
+        { }
+
+        public BookStoreException(string message) : base(message)
+        {
+        }
+
+        public BookStoreException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
